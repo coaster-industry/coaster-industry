48a061987ff39b03daf9b8501223c390a37c4ebe
lioneljung <lionel.jung@etu.unistra.fr>
2020-04-25 14:17:50 +0200

    init: add assembler layer and data

---
+++ app/data/assemblers.js
@@ -0,0 +1,17 @@
+var assemblers_geojson = 
+{
+    "type": "FeatureCollection",
+    "features": [ 
+        {
+            "category": "assembler",
+            "name": "Ride Construction Service worldwide (RCS) GmbH",
+            "brief": "The most known and most efficient roller coaster assembler.",
+            "uri": "#",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [10.3426574,53.3810499]
+            },
+            "links": []
+        }
+    ]
+};
